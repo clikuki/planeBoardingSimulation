6aa113d8a408896d55da4d3f539fd24f144cf27e
Lanz Vasquez <lanzvasquez24@gmail.com>
2025-02-05 15:06:50 +0800

    seat gen and drawing

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { HTML } from "./utils.js";
+import { HTML, shuffle } from "./utils.js";
 
 const canvas = HTML.getOne<HTMLCanvasElement>("canvas")!;
 const ctx = canvas.getContext("2d")!;
@@ -18,22 +18,54 @@ for (const side of [-1, 1]) {
 
 	planeOutline.moveTo(canvas.width - 300, side * (canvas.height / 2 + 10));
 	planeOutline.lineTo(canvas.width - 330, side * (corridorSize / 2));
+
+	const sideHeights = (side * corridorSize) / 2;
+	planeOutline.moveTo(0, sideHeights);
+	planeOutline.lineTo(canvas.width, sideHeights);
 }
-planeOutline.moveTo(0, -corridorSize / 2);
-planeOutline.lineTo(canvas.width, -corridorSize / 2);
-planeOutline.moveTo(0, corridorSize / 2);
-planeOutline.lineTo(canvas.width, corridorSize / 2);
 
 const planeBody = ctx.createLinearGradient(
 	0,
-	canvas.height / 2 - corridorSize / 2 - 40,
+	-corridorSize / 2 - 40,
 	0,
-	canvas.height / 2 + corridorSize / 2 + 40
+	+corridorSize / 2 + 40
 );
 planeBody.addColorStop(0, "transparent");
 planeBody.addColorStop(0.05, "#777");
 planeBody.addColorStop(0.95, "#777");
 planeBody.addColorStop(1, "transparent");
+
+interface Seat {
+	x: number;
+	y: number;
+}
+const seatSize = 68;
+const rowByRowPad = 20;
+const columnEdgePad = 15;
+const backSpace = 100;
+
+const rowSize = 3; // Num of side by side seats
+const colSize = 16; // Num of rows of seats
+const colCnt = 2; // Number of seating columns
+
+const rowSizePx = seatSize * rowSize + columnEdgePad * 2;
+const aisleSpace = (corridorSize - rowSizePx * colCnt) / (colCnt - 1);
+const seats: Seat[] = Array(colCnt * rowSize * colSize)
+	.fill(0)
+	.map((_, i) => {
+		const column = Math.floor((i % (rowSize * colCnt)) / rowSize);
+		const rowIndex = Math.floor(i / rowSize / colCnt);
+		const rowOffset = i % rowSize;
+
+		const rowPosition = rowIndex * (seatSize + rowByRowPad);
+		const colPosition = column * (rowSizePx + aisleSpace) + rowOffset * seatSize;
+		return {
+			id: String(i),
+			x: canvas.width - seatSize - backSpace - rowPosition + seatSize / 2,
+			y: columnEdgePad + colPosition - corridorSize / 2 + seatSize / 2,
+		};
+	});
+const seatIndices = new Map(seats.map((s, i) => [s, i]));
 
 function update() {}
 
@@ -42,17 +74,44 @@ function draw() {
 	ctx.fillStyle = "#112";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-	// Draw plane gradient
-	ctx.fillStyle = planeBody;
-	ctx.fillRect(0, corridorSize / 2 - 50, canvas.width, corridorSize + 100);
-
 	ctx.save();
 	ctx.translate(0, canvas.height / 2);
 
-	// Draw plane outline
+	// Plane gradient
+	ctx.fillStyle = planeBody;
+	ctx.fillRect(0, -canvas.height / 2, canvas.width, canvas.height);
+
+	// Plane outline
 	ctx.lineWidth = 10;
 	ctx.strokeStyle = planeStroke;
 	ctx.stroke(planeOutline);
+
+	// Seats
+	ctx.beginPath();
+	ctx.lineWidth = 5;
+	for (const { x, y } of seats) {
+		ctx.roundRect(
+			x - seatSize / 2 + 1 + ctx.lineWidth / 2 + 1,
+			y - seatSize / 2 + 1 + ctx.lineWidth / 2 + 1,
+			seatSize - ctx.lineWidth - 2,
+			seatSize - ctx.lineWidth - 2,
+			4
+		);
+	}
+	ctx.fillStyle = "#113";
+	ctx.strokeStyle = "#000";
+	ctx.fill();
+	ctx.stroke();
+
+	// Seat ID
+	ctx.fillStyle = "#449";
+	ctx.font = "20px monospace";
+	ctx.textAlign = "center";
+	ctx.textBaseline = "middle";
+	for (const seat of seats) {
+		const index = seatIndices.get(seat) ?? NaN;
+		ctx.fillText(String(index), seat.x, seat.y);
+	}
 
 	ctx.restore();
 }
--- src/utils.ts
+++ src/utils.ts
@@ -6,3 +6,16 @@ export const HTML = {
 		return Array.from(base.querySelectorAll(selectors)) as T[];
 	},
 };
+
+export function shuffle<T>(arr: T[]): T[] {
+	let i = arr.length,
+		j,
+		temp;
+	while (--i > 0) {
+		j = Math.floor(Math.random() * (i + 1));
+		temp = arr[j];
+		arr[j] = arr[i];
+		arr[i] = temp;
+	}
+	return arr;
+}
